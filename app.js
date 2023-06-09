e4348ba0b300e732a20eb0088fba0aba67cd5914
Francisco Manuel <fsaa93@gmail.com>
2023-06-09 09:31:32 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const path = "data.json";
 async function getData() {
     const res = await fetch(path);
     const data = await res.json();
-    console.log(res.results);
+    console.log(data.results.forEach(element => {
+        console.log(element.title)
+    }));
 }
 
 getData();
